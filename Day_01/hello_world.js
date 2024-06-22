870e2791f1a50d3c025e064f02f1594b7186f484
ehtisham-sadiq <bsef19m521@pucit.edu.pk>
2024-06-22 23:52:44 +0500

    day 01 content added

---
+++ Day_01/hello_world.js
@@ -0,0 +1,24 @@
+let a = "Ehtisham Sadiq"
+let b = "Mehwash Shahzadi"
+
+console.log(a + " is a good boy")
+console.log(b + " is a good girl")
+
+// example of a variable
+var c = 10
+console.log(c)
+
+let d = 20
+console.log(d)
+
+const e = "I am a constant";
+console.log(e)
+
+// difference between epression and statement
+// statement: 
+let f = 10
+let g = 20
+
+// expression:
+let h = f + g
+console.log(h) // calling the expression using console.log function
---
+++ Day_01/some.js
@@ -0,0 +1,3 @@
+console.log("I am in aonther file")
+const h1 = document.querySelector("h1");
+console.log(h1);
