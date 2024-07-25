552011b1bae3cc2522567620db1b7a1bac643aa6
ehtisham-sadiq <bsef19m521@pucit.edu.pk>
2024-07-25 15:19:59 +0500

    new topics added

--- OOP/05_Inheritance.js
+++ OOP/05_Inheritance.js
@@ -11,3 +11,33 @@
 // - Inheritance can lead to the diamond problem in multiple inheritance.
 // - Inheritance can make the code harder to understand and maintain.
 
+
+
+// Types of Inheritance in OOP:
+// - Single Inheritance: A child class inherits from a single parent class(GrandFather -> Father).
+// - Multilevel Inheritance: A child class inherits from another child class(GrndFather -> Father -> Son).
+// - Hierarchical Inheritance: Multiple child classes inherit from a single parent class(GrandFather -> Father, GrandFather -> Mother).
+// - Multiple Inheritance: A child class inherits from multiple parent classes(Father, Mother -> Child).
+// - Hybrid Inheritance: A combination of two or more types of inheritance. (Father -> Son, Mother -> Son, Son -> GrandSon).
+
+// Example of Single Inheritance:
+class Animal{
+    constructor(name){
+        this.name = name;
+    }
+    makeSound(){
+        return `${this.name} is making a sound`;
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, breed){
+        super(name); // here super() is used to call the constructor of the parent class.
+        this.breed = breed;
+    }
+    display(){
+        return `${this.name} is a ${this.breed}`;
+    }
+}
+
+const dog1 = new Dog('Buddy', 'Labrador');
